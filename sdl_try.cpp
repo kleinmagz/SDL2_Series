214124c2e3d287af70cfd92dc0d008714258b149
Klein Timothy <kleinalboriamagallano@gmail.com>
2025-02-18 13:25:05 +0800

    Memorizing SDL syntax. modified sdl_try.cpp and sdl_try.exe

--- sdl_try.cpp
+++ sdl_try.cpp
@@ -1,42 +1,44 @@
 #include <stdio.h>
 #include <SDL.h>
-#include <SDL_image.h>
+#include <SDL_Image.h>
 #include <string>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 680;
+const int SCREEN_HEIGHT = 460;
+
+SDL_Window* gwindow = NULL;
+SDL_Surface* gscreenSurface = NULL;
+SDL_Surface* PNGSurface = NULL;
+
+SDL_Surface* loadSurface( std::string path );
 
 bool init();
 bool loadMedia();
 void close();
 
-SDL_Window* gwindow = NULL;
-SDL_Surface* gscreenSurface = NULL;
-SDL_Surface* PNGSurface = NULL;
-SDL_Surface* loadImage( std::string path );
-
 bool init()
 {
   bool success = true;
-  if ( SDL_Init(SDL_INIT_VIDEO) < 0)
+  if ( SDL_Init(SDL_INIT_VIDEO) < 0 )
   {
-    printf("Failed to initialize SDL. SDL_error: %s \n", SDL_GetError());
+    printf("Failed to initialize SDL. SDL_Error: %s\n", SDL_GetError());
     success = false;
   }
   else
   {
-    gwindow = SDL_CreateWindow("SDL_TRY", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if( gwindow == NULL)
+    int imgFlag = IMG_INIT_PNG;
+    if( !(IMG_Init(imgFlag) & IMG_INIT_PNG) )
     {
-      printf("Failed to create window. SDL_Error: %s\n", SDL_GetError());
+      printf("Failed to initialize SDL_Image. IMG_Error: %s", IMG_GetError());
       success = false;
     }
     else
     {
-      int imageFlag = IMG_INIT_PNG;
-      if( !(IMG_Init(imageFlag) & IMG_INIT_PNG) )
+      gwindow = SDL_CreateWindow("SDL_TRY", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+      if ( gwindow == NULL)
       {
-        printf("Failed to initialize SDL_Image. IMG_Error: %s\n", IMG_GetError());
+        printf("Failed to create window. SDL_Error: %s\n", SDL_GetError());
         success = false;
       }
       else
@@ -51,11 +53,10 @@ bool init()
 bool loadMedia()
 {
   bool success = true;
-
-  PNGSurface = loadImage("./download.png");
-  if( PNGSurface == NULL)
+  PNGSurface = loadSurface("./download.png");
+  if ( PNGSurface == NULL)
   {
-    printf("Failed to load Image to surface. \n");
+    printf("Failed to load image. SDL_Error: %s\n", SDL_GetError());
     success = false;
   }
   return success;
@@ -73,22 +74,19 @@ void close()
   SDL_Quit();
 }
 
-SDL_Surface* loadImage( std::string path )
+SDL_Surface* loadSurface( std::string path )
 {
   SDL_Surface* optimizedSurface = NULL;
-  SDL_Surface* loadedSurface = IMG_Load( path.c_str());
-  if( loadedSurface == NULL )
+  SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+  if ( loadedSurface == NULL )
   {
     printf("Failed to load image from %s. IMG_Error: %s\n", path.c_str(), IMG_GetError());
   }
   else
   {
     optimizedSurface = SDL_ConvertSurface(loadedSurface, gscreenSurface->format, 0);
-    if( optimizedSurface == NULL )
-    {
-      printf("Failed to convert to optimized Surface. SDL_Error: %s\n", SDL_GetError());
-    }
-      SDL_FreeSurface(loadedSurface);
+    SDL_FreeSurface(loadedSurface);
+    loadedSurface = NULL;
   }
   return optimizedSurface;
 }
@@ -108,14 +106,13 @@ int main(int argc, char* args[])
     else
     {
       bool quit = false;
-
       SDL_Event event;
 
-      while(!quit)
+      while( !quit )
       {
         while( SDL_PollEvent(&event) != 0 )
         {
-          if( event.type == SDL_QUIT)
+          if (event.type == SDL_QUIT )
           {
             quit = true;
           }
@@ -126,7 +123,6 @@ int main(int argc, char* args[])
       }
     }
   }
-
   close();
   return 0;
 }
