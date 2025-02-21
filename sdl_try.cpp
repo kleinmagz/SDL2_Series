cd765ec86fb15061afcc94d81636ff854e83a066
Klein Timothy <kleinalboriamagallano@gmail.com>
2025-02-21 09:57:12 +0800

    Modified .vscode/seettings.json to add cmath functionality. Also experimented with drawing rectangles in SDL using 'primitive ' drawing functions using the renderer. Additionally, I compiled the said file to see the experiments I have done.

--- sdl_try.cpp
+++ sdl_try.cpp
@@ -1,21 +1,18 @@
-//TRYING TO MEMORIZE SDL_RENDERER
 #include <stdio.h>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <string>
+#include <cmath>
 
 const int SCREEN_WIDTH = 680;
-const int SCREEN_HEIGHT = 460;
+const int SCREEN_HEIGHT = 480;
 
 bool init();
 bool loadMedia();
 void close();
 
-SDL_Window* global_window = NULL;
-SDL_Renderer* global_renderer = NULL;
-SDL_Texture* PNG_Texture = NULL;
-
-SDL_Texture* loadTexture( std::string path );
+SDL_Window* gwindow = NULL;
+SDL_Renderer* grenderer = NULL;
 
 bool init()
 {
@@ -33,27 +30,26 @@ bool init()
       printf("Warning! Unable to set to linear texture filtering.\n");
     }
 
-    global_window = SDL_CreateWindow("SDL_TRY", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
-    if( global_window == NULL )
+    gwindow = SDL_CreateWindow("SDL_TRY", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if( gwindow == NULL )
     {
-      printf("Failed to create a window. SDL_Error: %s\n", SDL_GetError());
+      printf("Failed to create window. SDL_Error: %s\n", SDL_GetError());
       success = false;
     }
     else
     {
-      global_renderer = SDL_CreateRenderer(global_window, -1, SDL_RENDERER_ACCELERATED);
-      if( global_renderer == NULL )
+      grenderer = SDL_CreateRenderer(gwindow, -1, SDL_RENDERER_ACCELERATED);
+      if( grenderer == NULL )
       {
         printf("Failed to create renderer. SDL_Error: %s\n", SDL_GetError());
         success = false;
       }
       else
       {
-        SDL_SetRenderDrawColor(global_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_SetRenderDrawColor(grenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
         int imgFlags = IMG_INIT_PNG;
-        if (!IMG_Init(imgFlags) & imgFlags )
+        if( !IMG_Init(imgFlags) & imgFlags )
         {
           printf("Failed to initialize SDL_Image. IMG_Error: %s\n", IMG_GetError());
           success = false;
@@ -67,62 +63,29 @@ bool init()
 bool loadMedia()
 {
   bool success = true;
-
-  PNG_Texture = loadTexture("./download.png");
-  if ( PNG_Texture == NULL )
-  {
-    printf("Failed to load media.\n");
-    success = false;
-  }
-
   return success;
 }
 
 void close()
 {
-  SDL_DestroyTexture(PNG_Texture);
-  PNG_Texture = NULL;
-
-  SDL_DestroyRenderer(global_renderer);
-  global_renderer = NULL;
-
-  SDL_DestroyWindow(global_window);
-  global_window = NULL;
+  SDL_DestroyRenderer(grenderer);
+  grenderer = NULL;
+  SDL_DestroyWindow(gwindow);
+  gwindow = NULL;
 
   IMG_Quit();
   SDL_Quit();
 }
 
-SDL_Texture* loadTexture ( std::string path )
-{
-  SDL_Texture* loadedTexture = NULL;
-  SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-  if( loadedSurface == NULL )
-  {
-    printf("Failed to load surface from %s. IMG_Error: %s", path.c_str(), IMG_GetError());
-  }
-  else
-  {
-    loadedTexture = SDL_CreateTextureFromSurface( global_renderer, loadedSurface );
-    if ( loadedTexture == NULL )
-    {
-      printf("Failed to convert surface to texture. SDL_Error: %s", SDL_GetError());
-    }
-
-    SDL_FreeSurface(loadedSurface);
-  }
-  return loadedTexture;
-}
-
 int main(int argc, char* args[])
 {
-  if(!init())
+  if( !init() )
   {
     printf("Failed to initialize.\n");
   }
   else
   {
-    if(!loadMedia())
+    if( !loadMedia() )
     {
       printf("Failed to load media.\n");
     }
@@ -131,19 +94,38 @@ int main(int argc, char* args[])
       bool quit = false;
       SDL_Event event;
 
-      while ( !quit )
+      while( !quit )
       {
-        while ( SDL_PollEvent(&event) != 0 )
+        while( SDL_PollEvent(&event) != 0 )
         {
-          if ( event.type == SDL_QUIT )
+          if( event.type == SDL_QUIT)
           {
             quit = true;
           }
         }
+        SDL_Delay(500);
+        SDL_SetRenderDrawColor(grenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(grenderer);
+        SDL_RenderPresent(grenderer);
 
-        SDL_RenderClear(global_renderer);
-        SDL_RenderCopy(global_renderer, PNG_Texture, NULL, NULL);
-        SDL_RenderPresent(global_renderer);
+        SDL_Delay(500);
+        SDL_Rect rectangle = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+        SDL_SetRenderDrawColor(grenderer, 0xFF, 0x00, 0x00, 0xFF);
+        SDL_RenderFillRect(grenderer, &rectangle);
+        //SDL_Delay(2000);
+        SDL_RenderPresent(grenderer);
+
+        SDL_Delay(500);
+        SDL_SetRenderDrawColor(grenderer, 0x00, 0x00, 0x00, 0xFF);
+        SDL_RenderClear(grenderer);
+        SDL_RenderPresent(grenderer);
+
+        SDL_Delay(500);
+        SDL_Rect rectangle_outline = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3 , SCREEN_HEIGHT * 2 / 3 };
+        SDL_SetRenderDrawColor(grenderer, 0x00, 0xFF, 0x00, 0xFF);
+        SDL_RenderDrawRect(grenderer, &rectangle_outline);
+        SDL_RenderPresent(grenderer);
+
       }
     }
   }
